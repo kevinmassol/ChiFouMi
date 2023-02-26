9448ef3ddf599ce4c898bb5980745803d7a5e7a3
Kevin Massol <kevin.massol@icloud.com>
2023-02-26 08:54:32 +0100

    Initial Commit

---
+++ ChiFouMi/ChiFouMiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiFouMiApp.swift
+//  ChiFouMi
+//
+//  Created by KEVIN on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChiFouMiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
